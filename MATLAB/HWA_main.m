37af20d7b185c58652161a6ac655128062d6a93a
willpage32 <willpage32@gmail.com>
2017-05-02 12:53:21 +1000

    added RE data and a main

---
+++ MATLAB/HWA_main.m
@@ -0,0 +1,8 @@
+% William Page (587000) - Kevin Rassool (xxxxxx) ;
+% Semester 2 2017 - University of Melbourne        ; Started:     01/5/17
+% MCEN90018 - Advanced Fluid Dynamics              ; Last Edited: 19/5/17
+% Hot Wire Laboratory : Turbulent behaviours
+%
+% Main Script
+
+load('highRE.mat')
