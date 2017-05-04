2aeb3376dfa3e13180940425033cba818bed5a2f
willpage32 <willpage32@gmail.com>
2017-05-04 16:25:08 +1000

    First go at a surface fit for time

--- MATLAB/HWA_Calib_fitter.m
+++ MATLAB/HWA_Calib_fitter.m
@@ -2,42 +2,33 @@
 % Semester 2 2017 - University of Melbourne        ; Started:     01/5/17
 % MCEN90018 - Advanced Fluid Dynamics              ; Last Edited: 19/5/17
 % Hot Wire Laboratory : Turbulent behaviours
-% Curve Fitting: Using an exponential, 3rd order poly or sqrt fn
 %
-% FitType == 1 Provides a sqrt fit
+% Curve Fitting: Used for pre-cal and post cal
+%
+% FitType == 1 Provides a kings law fit
 % FitType == 2 Provides a 3rd order poly fit
 %
-% plotstho == 1 provides plots for the fits
-function V_handle = HWA_Calib_fitter(u,V,fitType,plotstho) 
+% Inputs are : HWA_Calib_fitter(VELOCITY,VOLTAGE,fitType) 
+
+function V_handle = HWA_Calib_fitter(u,V,fitType) 
 
 % Make a custom fit
-[xd1,yd1] = prepareCurveData(u ,V ) ; % Prepare data, strips inf/nan/etc
+[xd1,yd1] = prepareCurveData(u,V) ; % Prepare data, strips inf/nan/etc
+opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
 
 % Specify fit type
 if fitType==1
-    ft = fittype( 'a + b*((x)^(1/2))', 'independent', 'x', 'dependent', 'y' );
+    ft = fittype( '( app + bpp*x^(1/n) )', 'independent', 'x', 'dependent', 'y' );
+    opts.startPoint = [] % First guess for fit co-efficients
 elseif fitType == 2
     ft = fittype( 'a*x^3 + b*x^2 c*x + d', 'independent', 'x', 'dependent', 'y' );
+    opts.startPoint = [] % First guess for fit co-efficients
 else
     fprintf('Wrong input')
 end
 
-opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
-opts.startPoint = [u(1) V(1)]; % Make the first point extra correct
+
 
 % Make fits 
-[V_handle, ~] = fit( xd1, yd1 , ft, opts );
-
-% Plot the fit if asked to
-if strcmp(plotstho ,'yeahm8' )
-    u_fitdat = linspace(min(u), max(u), 1e2);
-    figure ; hold on ; 
-    plot(u,V); axis equal;
-    plot(u_fitdat,V_handle(u_fitdat)) ;
-    axis([0,14,-6,8]); 
-    title('Data from summary matrices') ; 
-    xlabel('u velocity') ; ylabel('Voltage');
-    legend('Original Data','Curve Fit');
-elseif strcmp(plotstho , 'nah')
-    
+[V_handle, ~] = fit( xd1, yd1 , ft ,opts);
 end
