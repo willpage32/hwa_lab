d560462df3c8c3743a0a97bfc73c91f65db6185f
willpage32 <willpage32@gmail.com>
2017-05-07 15:56:04 +1000

    more

--- MATLAB/high_freq.m
+++ MATLAB/high_freq.m
@@ -28,12 +28,19 @@ P_air   = 101325            ; % Ambient air pressure
 rho = P_air/(R*Temp)        ; % Fluid density -> (kg/m^3)
 mu  = 1.81e5 ; nu = mu/rho  ; % Viscocities
 
-up2n = 200;
-clip_1 = data_full(1:up2n,1);
-clip_2 = data_full(1:up2n,2);
+freq    = 30e3 ; % Data sampling frequency (for HWA!, not dyn pressure)
+up2time = .1   ; % Time up to take the data 
+up2n = freq*up2time ; % Number of elements to pull out of the arrays
+clip_1 = data_full(1:up2n,1) ; % First  row of clipped data
+clip_2 = data_full(1:up2n,2) ; % Second row of clipped data
 
-figure ; hold on ; plot(clip_1) ; title('.daq file data, column 1, first 200 points') ;
-figure ; plot(clip_2) ;title('.daq file data, column 2,first 200 points') ;
+figure ; plot(clip_1) ; 
+title('.daq file data, column 1, (HWA Voltage I think?') ;
+xlabel('Time (1/30000 s)') ; ylabel('Voltage (V)') ;
+
+figure ; plot(clip_2) ;
+title('.daq file data, column 2,first 200 points') ;
+xlabel('Time (1/30000 s)') ; ylabel('Dynamic pressure') ;
 %  xlabel('Sample Number') ; ylabel('who knows') ;
 % legend('Row 1','Row 2')
 
